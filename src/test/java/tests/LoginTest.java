3df91d0ed4743d217c6a17a99d622bc59c742128
haule2020 <huyhau2015@gmail.com>
2026-01-20 13:00:36 +0700

    demo Jenkins

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,12 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,9 +21,17 @@ public class LoginTest extends BaseTest{
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		loginPage.clickLogin();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement dashboard = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()=' Dashboard']")));
+		
+		String url = driver.getCurrentUrl();
+		
+		System.out.print("url is : " + url);
+		
 		System.out.print("title is : " + driver.getTitle());
 		
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
 	}
 	
 }
